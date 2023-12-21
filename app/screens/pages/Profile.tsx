a67a9b218e006afea5f02167e94c285c3e6f3500
Param <param23403@gmail.com>
2023-12-21 10:34:41 -0500

    temp profile changes

--- app/screens/pages/Profile.tsx
+++ app/screens/pages/Profile.tsx
@@ -1,12 +1,181 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import Navbar from '../../../components/Navbar';
+import React, {useEffect, useState } from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import pfp from '../../assets/Profile.png'; // Importing the profile picture
+import arrow from '../../assets/arrow_back_ios_new.png'; // Importing the back arrow
 
-const Profile = () => {
-    return (
-        <View>
-        </View>
-    );
+import ProfileEditPage from './ProfileEditPage'; // Update the path accordingly
+
+
+import { useNavigation } from '@react-navigation/native';
+
+
+import * as Font from 'expo-font';
+
+
+type Props = {};
+
+
+const ProfilePage: React.FC<Props> = () => {
+
+    const navigation = useNavigation();
+    
+    const [fontsLoaded, setFontsLoaded] = useState(false);
+
+   const userData = {
+       name: "Varizzy Rizzy",
+       email: "vrizz@gmail.com",
+       hoocoins: "12",
+       foodWave: "A",
+   };
+
+
+   const handleEditProfilePress = () => {
+       console.log('Edit Profile Pressed');
+       // go to EditProfile.tsx
+       navigation.navigate('ProfileEditPage');
+   };
+
+   useEffect(() => {
+    const loadFonts = async () => {
+      try {
+        await Font.loadAsync({
+          'ChakraPetch-Bold': require('../../assets/Chakra_Petch/ChakraPetch-Bold.ttf'),
+          'ChakraPetch-Light': require('../../assets/Chakra_Petch/ChakraPetch-Light.ttf'),
+          'ChakraPetch-Regular': require('../../assets/Chakra_Petch/ChakraPetch-Regular.ttf'),
+        });
+  
+        setFontsLoaded(true);
+      } catch (error) {
+        console.error('Error loading fonts:', error);
+      }
+    };
+  
+    loadFonts();
+    }, []);
+    
+    if (!fontsLoaded) {
+        return null;
+    }
+
+
+   return (
+       <View style={styles.container}>
+           <TouchableOpacity style={styles.backButton} onPress={handleEditProfilePress}>
+               <Image
+                   source={arrow}
+                   style={styles.backArrow}
+               />
+           </TouchableOpacity>
+           <View style={styles.header}>
+               <Image
+                   source={pfp}
+                   style={styles.profileImage}
+               />
+               <View style={styles.userInfo}>
+                   <View style={{flexDirection: 'row'}}><Text style={styles.title}>Name: </Text><Text style={styles.name}>{userData.name}</Text></View>
+                   <View style={{flexDirection: 'row'}}><Text style={styles.title}>Email: </Text><Text style={styles.email}>{userData.email}</Text></View>
+                   <View style={{flexDirection: 'row'}}><Text style={styles.title}>Hoocoins: </Text><Text style={styles.hoocoins}>{userData.hoocoins}</Text></View>
+                   <View style={{flexDirection: 'row'}}><Text style={styles.title}>Food Wave: </Text><Text style={styles.foodWave}>{userData.foodWave}</Text></View>
+               </View>
+           </View>
+           <TouchableOpacity style={styles.editButton} onPress={handleEditProfilePress}>
+               <Text style={styles.editButtonText}>EDIT ACCOUNT</Text>
+           </TouchableOpacity>
+       </View>
+   );
 };
 
-export default Profile;
+
+const styles = StyleSheet.create({
+   container: {
+       flex: 1,
+       backgroundColor: '#FFFFFF',
+       paddingTop: 50,
+   },
+   backButton: {
+       position: 'absolute',
+       top: 10,
+       left: 10,
+       zIndex: 1,
+   },
+   backArrow: {
+       width: 25,
+       height: 25,
+   },
+   title: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#121A6A',
+    fontFamily: 'ChakraPetch-Regular',
+   },
+   header: {
+       width: '90%',
+       backgroundColor: '#E5EEFF',
+       borderRadius: 5,
+       alignItems: 'center',
+       justifyContent: 'center',
+       paddingVertical: 20,
+       flexDirection: 'row',
+       alignSelf: 'center',
+       shadowColor: '#000',
+       shadowOffset: { width: 1, height: 2 },
+       shadowOpacity: .6,
+       shadowRadius: 2.5,
+       elevation: 3,
+   },
+   profileImage: {
+       width: 100,
+       height: 100,
+       borderRadius: 50,
+       marginHorizontal: 20,
+   },
+   userInfo: {
+       flex: 1,
+   },
+   name: {
+       fontSize: 16,
+       fontWeight: 'bold',
+       color: '#121A6A',
+       fontFamily: 'ChakraPetch-Bold',
+   },
+   email: {
+       fontSize: 16,
+       color: '#121A6A',
+       fontFamily: 'ChakraPetch-Bold',
+   },
+   hoocoins: {
+       fontSize: 16,
+       color: '#121A6A',
+       fontFamily: 'ChakraPetch-Bold',
+   },
+   foodWave: {
+       fontSize: 16,
+       color: '#121A6A',
+       fontFamily: 'ChakraPetch-Bold',
+   },
+   editButton: {
+       width: '90%',
+       alignSelf: 'center',
+       backgroundColor: '#FFFFFF',
+       borderRadius: 5,
+       paddingVertical: 10,
+       paddingHorizontal: 20,
+       marginTop: 20,
+       shadowColor: '#000',
+       shadowOffset: { width: 1, height: 3 },
+       shadowOpacity: 0.6,
+       shadowRadius: 2.5,
+       elevation: 3,
+   },
+   editButtonText: {
+       textAlign: 'center',
+       color: '#121A6A',
+       fontSize: 16,
+       fontWeight: 'bold',
+       fontFamily: 'ChakraPetch-Bold',
+
+   },
+});
+
+
+export default ProfilePage;
