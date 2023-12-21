6c9de84c997589663a687d7afb06b60506c7d032
Param <param23403@gmail.com>
2023-12-21 11:19:18 -0500

    global styles added

---
+++ app/screens/pages/GlobalStyles.ts
@@ -0,0 +1,30 @@
+/* fonts */
+export const FontFamily = {
+  chakraPetchRegular: "ChakraPetch-Regular",
+  chakraPetchSemiBold: "ChakraPetch-SemiBold",
+  sFPro: "SF Pro",
+  chakraPetchBold: "ChakraPetch-Bold",
+  robotoRegular: "Roboto-Regular",
+};
+/* font sizes */
+export const FontSize = {
+  size_base: 16,
+  size_xl: 20,
+  size_mid: 17,
+  size_5xl: 24,
+  size_sm: 14,
+};
+/* Colors */
+export const Color = {
+  colorWhite: "#fff",
+  colorMidnightblue: "#121a6a",
+  colorLavender: "rgba(229, 238, 255, 0.01)",
+  colorBlack: "#000",
+  colorLightskyblue: "#b1ccff",
+};
+/* border radiuses */
+export const Border = {
+  br_3xs: 10,
+  br_10xs_5: 3,
+  br_8xs_3: 4,
+};
