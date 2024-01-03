76131c5a69ebc90f5e7a1de576150a1cf4d9ac88
poudyalankit <poudyalankit@gmail.com>
2024-01-03 01:32:14 -0500

    fix layout

--- App.tsx
+++ App.tsx
@@ -43,16 +43,20 @@ const WebLayout = () => {
     );
 };
 
-const UserLayout = () => {
+function UserLayout() {
     return isWeb ? <WebLayout /> : (
         <Tab.Navigator tabBar={(props) => <Navbar {...props} />}>
             <Tab.Screen name="Home" component={Home} />
             <Tab.Screen name="Schedule" component={Schedule} />
             <Tab.Screen name="Leaderboard" component={Leaderboard} />
             <Tab.Screen name="Profile" component={Profile} />
+            <Tab.Screen name="ProfileEditPage" component={ProfileEditPage} />
+            <Stack.Screen name="ReferFriend" component={ReferFriend} options={{headerShown:false}}/>
+            <Stack.Screen name="ViewParticipants" component={ViewParticipants} />
+
         </Tab.Navigator>
     );
-};
+}
 
 // views
 import Details from "./app/screens/Details";
@@ -68,102 +72,102 @@ const AuthStack = createNativeStackNavigator();
 
 //function MenuLayout() {
 //   return (
-    // // add your own screen here in same format
-    // <MenuStack.Navigator>
-    //   <MenuStack.Screen
-    //     name="Detail"
-    //     component={Details}
-    //   />
-    //   {/* <UserStack.Screen
-    //     name="Application"
-    //     component={Application}
-    //   /> */}
-    // </MenuStack.Navigator>
-  // );
+// // add your own screen here in same format
+// <MenuStack.Navigator>
+//   <MenuStack.Screen
+//     name="Detail"
+//     component={Details}
+//   />
+//   {/* <UserStack.Screen
+//     name="Application"
+//     component={Application}
+//   /> */}
+// </MenuStack.Navigator>
+// );
 // };
 
 function AuthLayout() {
-  return (
-    <AuthStack.Navigator
-      initialRouteName="login"
-    >
-      <AuthStack.Screen
-        name="signup"
-        component={SignUp}
-        options={{
-          headerShown:false,
-          title: "HackX - Sign Up"
-        }}
-      />
-      <AuthStack.Screen
-        name="login"
-        component={Login}
-        options={{
-          headerShown:false,
-          title: "HackX - Login"
-        }}
-      />
-      <AuthStack.Screen
-        name="reset-password"
-        component={PasswordReset}
-        options={{
-          headerShown:false,
-          title: "HackX - Password Reset"
-        }}
-      />
-    </AuthStack.Navigator>
-  )
+    return (
+        <AuthStack.Navigator
+            initialRouteName="login"
+        >
+            <AuthStack.Screen
+                name="signup"
+                component={SignUp}
+                options={{
+                    headerShown:false,
+                    title: "HackX - Sign Up"
+                }}
+            />
+            <AuthStack.Screen
+                name="login"
+                component={Login}
+                options={{
+                    headerShown:false,
+                    title: "HackX - Login"
+                }}
+            />
+            <AuthStack.Screen
+                name="reset-password"
+                component={PasswordReset}
+                options={{
+                    headerShown:false,
+                    title: "HackX - Password Reset"
+                }}
+            />
+        </AuthStack.Navigator>
+    )
 }
 
 const App = () => {
 
-  const [user, setUser] = useState<User | null>(null);
+    const [user, setUser] = useState<User | null>(null);
 
-  useEffect(() => {
-    onAuthStateChanged(FIREBASE_AUTH, (user) => {
-      setUser(user);
-    });
-  }, []);
+    useEffect(() => {
+        onAuthStateChanged(FIREBASE_AUTH, (user) => {
+            setUser(user);
+        });
+    }, []);
 
-  const linking = {
-    prefixes: ['http://localhost:19006', 'project://'],
-    config: {
-      screens: {
-        Test: '/test',
-        Search: 'search',
-        Login: '/login',
-        SignUp: '/sign-up',
-        Detail: '/detail',
-        NotFound: '/404'
-      },
-    },
-  };
+    const linking = {
+        prefixes: ['http://localhost:19006', 'project://'],
+        config: {
+            screens: {
+                Test: '/test',
+                Search: 'search',
+                Login: '/login',
+                SignUp: '/sign-up',
+                Detail: '/detail',
+                NotFound: '/404'
+            },
+        },
+    };
 
-  return (
-    <NavigationContainer
-      linking={linking}
-    >
-      <Stack.Navigator>
-        {user ? (
-          <Stack.Screen
-            name="user"
-            component={UserLayout}
-            options={{
-              headerShown: false
-            }}
-          />
-        ) : (
-          <Stack.Screen
-            name="auth"
-            component={AuthLayout}
-            options={{
-              headerShown:false
-            }}
-          />
-        )}
-      </Stack.Navigator>
-    </NavigationContainer>
-  )
+    return (
+        <NavigationContainer
+            linking={linking}
+        >
+            <Stack.Navigator>
+                {user ? (
+                    <Stack.Screen
+                        name="user"
+                        component={UserLayout}
+                        options={{
+                            headerShown: false
+                        }}
+                    />
+                ) : (
+                    <Stack.Screen
+                        name="auth"
+                        component={AuthLayout}
+                        options={{
+                            headerShown:false
+                        }}
+                    />
+                )}
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
 }
 
 const styles = StyleSheet.create({
