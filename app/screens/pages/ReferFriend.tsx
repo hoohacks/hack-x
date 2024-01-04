eafd1592af659d8a9d559bf5d5425e7f6266861e
Param <param23403@gmail.com>
2024-01-04 16:04:46 -0500

    referafriend coming soon

--- app/screens/pages/ReferFriend.tsx
+++ app/screens/pages/ReferFriend.tsx
@@ -1,12 +1,34 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
-import Navbar from '../../../components/Navbar';
+import { View, Text, Button,StyleSheet } from "react-native"
+import { getAuth } from "firebase/auth";
+import { NavigationProp } from "@react-navigation/native";
+import { Color, Border, FontFamily, FontSize } from "./GlobalStyles";
 
-const ReferFriend = () => {
-    return (
-        <View>
-        </View>
-    );
+interface RouterProps {
+  navigation: NavigationProp<any, any>;
+}
+
+const ReferFriend = ({ navigation }: RouterProps) => {
+
+  const auth = getAuth();
+
+  return (
+    <View>
+      <Text style={styles.comingSoon}>Coming Soon...</Text>
+    </View>
+  );
 };
-
+const styles= StyleSheet.create({
+    comingSoon: {
+        fontWeight: "700",
+        fontFamily: FontFamily.chakraPetchBold,
+        fontSize: FontSize.size_5xl,
+        textAlign: "center",
+        color: Color.colorBlack,
+        lineHeight: 22,
+        top: 0,
+        left: 0,
+        position: "absolute",
+      },
+})
 export default ReferFriend;
+
